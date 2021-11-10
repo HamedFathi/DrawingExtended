f34ff5de4bb68c71b7797ff95fadc9a989062da2
hamedfathi <hamedfathi@outlook.com>
2021-11-10 15:41:25 +0330

    feat: functionalities added.

---
+++ DrawingExtended/ColorExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace DrawingExtended
+{
+    public static class ColorExtensions
+    {
+        public static string ToHtmlColor(this Color color)
+        {
+            return ColorTranslator.ToHtml(color);
+        }
+
+        public static int ToOleColor(this Color color)
+        {
+            return ColorTranslator.ToOle(color);
+        }
+
+        public static int ToWin32Color(this Color color)
+        {
+            return ColorTranslator.ToWin32(color);
+        }
+    }
+}
